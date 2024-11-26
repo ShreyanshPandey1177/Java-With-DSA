003e07ab35fac7b4827c9e2e2539b79d490e1938
Shreyansh Pandey <149287471+ShreyanshPandey1177@users.noreply.github.com>
2024-11-26 11:56:59 +0530

    Add files via upload

---
+++ 2-D array/SpiralMatrix.java
@@ -0,0 +1,47 @@
+public class SpiralMatrix {
+
+    public static void printSpiral(int matrix[][]){
+        int startRow=0;
+        int startCol=0;
+        int endRow=matrix.length-1;
+        int endCol=matrix[0].length-1;
+
+        while (startRow<=endRow && startCol<=endCol) {
+            //Top boundary
+            for(int j=startCol;j<=endCol;j++){
+                System.out.println(matrix[startRow][j]+" ");
+            }
+
+            //Right boundary
+            for(int i= startRow+1;i<=endRow;i++){
+                System.out.println(matrix[i][endCol]+" ");
+            }
+
+            //bottom boundary 
+             for(int j=endCol-1;j>=startCol;j--){
+                if (startCol==endCol) {
+                    break;
+                }
+                System.out.println(matrix[endRow][j]+" ");
+            }
+
+            //left boundary
+            for(int i=endRow-1;i>=startRow;i--){
+                System.out.println(matrix[i][startCol]+" ");
+            }
+
+            startCol++;
+            startRow++;
+            endCol--;
+            endRow--;
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int matrix[][]={{1,2,3,4},
+                        {5,6,7,8},
+                        {9,10,11,12},
+                        {13,14,15,16}};
+        printSpiral(matrix);
+    }
+}
